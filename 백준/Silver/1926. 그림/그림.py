6666d0e49ec26c56965710445f2c32255ba58ba6
nosong2 <157494028+nosong2@users.noreply.github.com>
2024-03-11 00:14:33 +0900

    [Silver I] Title: 그림, Time: 248 ms, Memory: 142748 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,44 +1,44 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
 di = [0, 1, 0, -1]
 dj = [1, 0, -1, 0]
 
-def find_picture():
+def bfs():
+    global picture_size_save, picture_count
     for i in range(N):
         for j in range(M):
             if paper[i][j] == 1 and visited[i][j] == 0:
                 paper_list.append([i, j])
-
-def dfs(i, j):
-    global picture_size, picture_size_save, picture_count
-    if visited[i][j] != 0:
-        picture_count -= 1
-        return
-    picture_count += 1
-    if picture_size_save < picture_size:
-        picture_size_save = picture_size
-    visited[i][j] = 1
-    for k in range(4):
-        ni = i + di[k]
-        nj = j + dj[k]
-        if 0 <= ni < N and 0 <= nj < M and paper[ni][nj] == 1 and visited[ni][nj] == 0:
-            picture_size += 1
-            dfs(ni, nj)
+    while paper_list:
+        q, w = paper_list.popleft()
+        if visited[q][w] == 0:
+            Queue.append([q, w])
+            visited[q][w] = 1
+            picture_count += 1
+            picture_size = 1
+            while Queue:
+                y, x = Queue.popleft()
+                for k in range(4):
+                    ni = y + di[k]
+                    nj = x + dj[k]
+                    if 0 <= ni < N and 0 <= nj < M and paper[ni][nj] == 1 and visited[ni][nj] == 0:
+                        Queue.append([ni, nj])
+                        visited[ni][nj] = visited[y][x] + 1
+                        picture_size += 1
+                if picture_size_save < picture_size:
+                    picture_size_save = picture_size
 
 
 N, M = map(int, input().split())
 paper = [list(map(int, input().split())) for _ in range(N)]
 visited = [[0] * M for _ in range(N)]
+Queue = deque()
 paper_list = deque()
 picture_count = 0
 picture_size_save = 0
-find_picture()
-while paper_list:
-    picture_size = 1
-    y, x = paper_list.popleft()
-    dfs(y, x)
+
+bfs()
 print(picture_count)
 print(picture_size_save)
