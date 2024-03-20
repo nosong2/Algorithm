5443a2f64ad15838e56b670dea60d26cd184dcbe
nosong2 <157494028+nosong2@users.noreply.github.com>
2024-03-20 21:28:18 +0900

    [Silver III] Title: N과 M (6), Time: 156 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+def dd(level, Start):
+    if level == M:
+        print(*path)
+        return
+    for i in range(Start, N):
+        if visited[i] == 1:continue
+        visited[i] = 1
+        path.append(oh[i])
+        dd(level + 1, i)
+        path.pop()
+        visited[i] = 0
+
+
+N, M = map(int, input().split())
+oh = list(map(int, input().split()))
+oh.sort()
+visited = [0] * N
+path = []
+
+dd(0, 0)
