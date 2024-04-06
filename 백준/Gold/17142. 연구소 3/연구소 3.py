9dbbce7a3f0470163f611a6a0f092a36fdac0619
nosong2 <157494028+nosong2@users.noreply.github.com>
2024-04-06 16:34:14 +0900

    [Gold III] Title: 연구소 3, Time: 248 ms, Memory: 114796 KB -BaekjoonHub

---
+++ 백준/Gold/17142. 연구소 3/연구소 3.py
@@ -0,0 +1,72 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+di = [0, 1, 0, -1]
+dj = [1, 0, -1, 0]
+
+def bfs():
+    global zero_cnt, result
+    non_virus = zero_cnt
+    visited = [[0] * N for _ in range(N)]
+    Queue = deque(path)
+    while Queue:
+        by, bx = Queue.popleft()
+        if visited[by][bx] == 0: visited[by][bx] = 1
+        for k in range(4):
+            ni = by + di[k]
+            nj = bx + dj[k]
+            if 0 <= ni < N and 0 <= nj < N and visited[ni][nj] == 0:
+                if laboratory[ni][nj] == 0:
+                    visited[ni][nj] = visited[by][bx] + 1
+                    non_virus -= 1
+                    Queue.append([ni, nj])
+                elif laboratory[ni][nj] == 2:
+                    visited[ni][nj] = visited[by][bx] + 1
+                    Queue.append([ni, nj])
+
+                if non_virus == 0:
+                    result = min(result, visited[ni][nj] - 1)
+                    return
+
+def jo(level, Start):
+    if level == M:
+        bfs()
+        return
+    for q in range(Start, len(virus)):
+        y, x = virus[q]
+        laboratory[y][x] = 'V'
+        path.append([y, x])
+        jo(level + 1, q + 1)
+        path.pop()
+        laboratory[y][x] = 2
+
+
+
+
+
+N, M = map(int, input().split())
+laboratory = [list(map(int, input().split())) for _ in range(N)]
+zero_cnt = 0
+virus = []
+path = deque()
+result = 9999999999
+
+for i in range(N):
+    for j in range(N):
+        if laboratory[i][j] == 0:
+            zero_cnt += 1
+        elif laboratory[i][j] == 2:
+            virus.append([i, j])
+if M == 0:
+    print(0)
+    exit(0)
+elif zero_cnt == 0:
+    print(0)
+    exit(0)
+
+jo(0, 0)
+if result == 9999999999:
+    print(-1)
+else:
+    print(result)
