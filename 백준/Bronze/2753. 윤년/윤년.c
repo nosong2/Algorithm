752a170e3ceeee94b0bc0918fa1162aa5b3077ee
nosong2 <157494028+nosong2@users.noreply.github.com>
2025-07-16 14:11:51 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.c
+++ 백준/Bronze/2753. 윤년/윤년.c
@@ -6,10 +6,7 @@ int main() {
 
   scanf("%d", &year);
 
-  if (year % 4 == 0 && year % 100 != 0) {
-    answer = 1;
-  }
-  else if (year % 400 == 0) {
+  if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
     answer = 1;
   }
 
