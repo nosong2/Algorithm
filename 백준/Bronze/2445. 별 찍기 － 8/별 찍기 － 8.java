8401575050600737fbebf5ee0ced0fb6905c15bb
nosong2 <157494028+nosong2@users.noreply.github.com>
2025-07-18 20:35:26 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 112 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            bw.write("*".repeat(i + 1));
+            bw.write(" ".repeat(n - i - 1));
+            bw.write(" ".repeat(n - i - 1));
+            bw.write("*".repeat(i + 1));
+            bw.newLine();
+        }
+        for (int i = 0; i < n; i++) {
+            bw.write("*".repeat(n - i - 1));
+            bw.write(" ".repeat(i + 1));
+            bw.write(" ".repeat(i + 1));
+            bw.write("*".repeat(n - i - 1));
+            bw.newLine();
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
