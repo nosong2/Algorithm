be0de6ba21ad82b70c674626257087833863df85
nosong2 <157494028+nosong2@users.noreply.github.com>
2025-07-19 01:24:51 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  int t;
+  scanf("%d", &t);
+
+  for (int i = 1; i <= t; i++) {
+    for (int k = t - 1; k >= i; k--) {
+      printf("%s", " ");
+
+    }
+    for (int j = 0; j < i; j++) {
+      printf("%s", "*");
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
