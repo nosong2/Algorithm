19a1a6f03eb1550384f84429b1d947beb214041f
nosong2 <157494028+nosong2@users.noreply.github.com>
2025-07-18 20:18:33 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        for (int i = n; i > 0; i--) {
+            bw.write(" ".repeat(n - i));
+            bw.write("*".repeat(i));
+            bw.newLine();
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
