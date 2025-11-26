99ab6b59c017bcd3a6c020b39dc3ad56e89cbd11
nosong2 <157494028+nosong2@users.noreply.github.com>
2025-11-26 16:01:42 +0900

    [Silver III] Title: 소수 구하기, Time: 892 ms, Memory: 110736 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,15 @@
+import sys
+
+x, y = map(int, input().split())
+cnt = 0
+
+for i in range(x, y + 1):
+    if i == 1:
+        continue
+    for j in range(2, int(i**0.5) + 1):
+        if i % j == 0:
+            break
+    else:
+        print(i)
+    # if now == True:
+    #     print(i)
