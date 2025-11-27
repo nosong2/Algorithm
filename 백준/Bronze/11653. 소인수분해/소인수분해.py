cd8a6402416aaeb6cf405815cb2d6f1b6da63cdd
nosong2 <157494028+nosong2@users.noreply.github.com>
2025-11-27 20:03:50 +0900

    [Bronze I] Title: 소인수분해, Time: 192 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,18 +1,10 @@
 import sys
 
-x = int(input())
-back = True
-
-while True:
-    for i in range(2, x + 1):
-        if x % i == 0:
-            x = x // i
-            print(i)
-            break
-    if x == 1:
-        back = False
-    elif x == 2 or x == 3:
-        print(x)
-        back = False
-    if not back:
-        break
+num = int(input())
+i = 2
+while num > 1:
+    if num % i == 0:
+        num = num // i
+        print(i)
+    else:
+        i += 1
